a98bea260a8e5c539e2bd526078a788f6a7b3d25
Omarespo-dev <omaresposito21@gmail.com>
2025-01-13 15:50:06 +0100

    add logic button

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -4,5 +4,53 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 
+
+// chieda all’utente: - Il numero di chilometri
+const chiediKilometri=parseInt("Inserisci il numero di km")
+// console.log(chiediKilometri)
+
+
+
+// chiedi all'utente l'età
+const chiediEtà=parseInt("Inserisci la tua età")
+// console.log(chiediEtà)
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const prezzoAlkm= 0.21 *chiediKilometri
+// console.log(prezzoAlkm.toFixed(2) + " €")
+
+// Prezzo scontato 20% minorenni
+const prezzoScontato1= prezzoAlkm * 0.20;
+const risultatoSconto1=prezzoAlkm - prezzoScontato1;
+
+// Prezzo scontato 40% minorenni
+const prezzoScontato2= prezzoAlkm * 0.40;
+const risultatoSconto2=prezzoAlkm - prezzoScontato2;
+
+// Imposto condizione 
+
+if(chiediEtà < 18){
+    console.log("Sei minorenne hai diritto solo al 20% di sconto sul biglietto","Pagherai:", risultatoSconto1.toFixed(2) + " €")
+}else if(chiediEtà > 65){
+    console.log("Hai superato i 65 anni, hai diritto a uno sconto del 40% sul biglietto","Pagherai:", risultatoSconto2.toFixed(2) + " €")
+}else{
+    console.log(" Mi dispiace non hai diritto a nessun sconto! ")
+}
+
+
+
+
+
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+// Dom accesso element
+const età = document.getElementById("input1")
+const km = document.getElementById("input2")
+const button = document.getElementById("set-button")
+const form = document.getElementById("set-form")
+
+// evento al form per non far ricarica la pagina
+form.addEventListener("submit", function(event){
+    event.preventDefault();  //Gli diciamo
+})
